569dd3ec3638bf9381defbd163f66e76964b5e48
Tyler Moline <tylermoline95@gmail.com>
2025-01-12 19:10:58 -0700

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import WhosThatPokemon from "./components/WhosThatPokemon";
 import CatchThePokemon from "./components/CatchThePokemon";
 import PokemonMemoryMatch from "./components/PokemonMemoryMatch";
+import HomePage from "./components/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       {/* <WhosThatPokemon /> */}
       {/* <CatchThePokemon /> */}
-      <PokemonMemoryMatch />
+      {/* <PokemonMemoryMatch /> */}
+
+      <Router>
+        <nav>
+          <Link to="/pokemon-games/">Home</Link>
+          <Link to="/pokemon-games/whos-that-pokemon">Whos that pokemon</Link>
+          <Link to="/pokemon-games/pokemon-memory-match">Memory Match</Link>
+          <Link to="/pokemon-games/catch-the-pokemon">Catch The Pokemon</Link>
+        </nav>
+        <Routes>
+          <Route path="/pokemon-games" element={<HomePage />} />
+          <Route
+            path="/pokemon-games/whos-that-pokemon"
+            element={<WhosThatPokemon />}
+          />
+          <Route
+            path="/pokemon-games/pokemon-memory-match"
+            element={<PokemonMemoryMatch />}
+          />
+          <Route path="/catch-the-pokemon" element={<CatchThePokemon />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        Welcome to our Pokémon web app, a place where fans of all ages can
+        immerse themselves in the exciting world of Pokémon! Explore fun and
+        interactive games designed to challenge your skills, spark your
+        creativity, and bring the adventure to life. Whether you're
+        strategizing, testing your knowledge, or just enjoying the vibrant
+        Pokémon universe, there's something here for everyone. Get ready to dive
+        in and embark on your next Pokémon adventure!
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
